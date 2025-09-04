60f570c99f5e6ff4e3fc15ae92f4fce06a69a537
di-shykh <di49@mail.ru>
2025-09-04 13:31:49 +0300

    feat: add task 01 lesson 8

---
+++ codewars/8ky/sum-mixed-array.js
@@ -0,0 +1,9 @@
+/*Description:
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.*/
+function sumMix(x) {
+  const initialValue = 0;
+  const sum = x.reduce((accumulator, currentValue) => +accumulator + (+currentValue), initialValue);
+  return sum;
+}
